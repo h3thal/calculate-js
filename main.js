940946383ca06eea7b513cb0f9daa56fbf2fa8e5
h3thal <h3thal@yandex.ru>
2021-06-02 11:19:52 +0300

    add html template

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 let arrMathOperation = ['+','-', '*', '/'];
-let arrNumber = [...Array(10).keys()];
+let arrNumber = [...Array.from({length: 9}, (v, k) => k + 1 ), 0];
 let arrNumbAndMath = [...arrMathOperation, ...arrNumber];
 var arrObjectButton = [];
+const template = `
+    <div id='calc'>
+        <input type='number'/>
+    </div>
+`;
 
-class Button {
+class Calculate {
+    constructor () {
+        document.body.insertAdjacentHTML('beforeend', template);
+        arrNumbAndMath.forEach(element => {
+            new CalculateButton(element);
+        });
+    }
+}
+
+class CalculateButton {
     constructor (text) {
         this.text = text;
         let elem =  document.createElement('button');
         this.getElem = elem;
         elem.innerText = text;
-        document.body.append(elem);
+        calc.append(elem);
         elem.addEventListener('click', () => {
             console.log(this.text);
         });
         arrObjectButton.push(this);
     }
 }
-arrNumbAndMath.forEach(element => {
-    new Button(element);
-});
+
 // создать свою функцию eval с помощью регулярки
+
+new Calculate();
